86d66ead07dbd556e4610f32a15e3006cfc2fc72
Dominik Gębski <domgebski@gmail.com>
2021-11-30 16:10:53 +0100

    Add react app, connect with api server, set redux, axios

--- run.sh
+++ run.sh
@@ -1,3 +1,5 @@
-gnome-terminal --tab -e '/bin/bash -c "cd client; yarn install; yarn start";bash'
 docker build -t fastapi-starter .
-docker run -p 8099:8000 fastapi-starter
+docker run -dp 8099:8000 fastapi-starter
+cd client
+yarn install
+yarn start
